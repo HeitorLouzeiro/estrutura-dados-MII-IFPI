71b6118c2aed778c56739ee83725ea1c521c7f2e
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2023-10-05 10:16:49 -0300

    Switch case activity carried out in the classroom.

---
+++ AtividadesSala/Aula0510.c
@@ -0,0 +1,38 @@
+/* Contruir um progama que verifica o dia da semana de acordo com o número digitado pelo usuário. */
+#include <stdio.h>
+
+int main()
+{
+    int dia;
+
+    printf("Digite um numero de 1 a 7: ");
+    scanf("%d", &dia);
+
+    switch (dia)
+    {
+    case 1:
+        printf("Domingo");
+        break;
+    case 2:
+        printf("Segunda");
+        break;
+    case 3:
+        printf("Terca");
+        break;
+    case 4:
+        printf("Quarta");
+        break;
+    case 5:
+        printf("Quinta");
+        break;
+    case 6:
+        printf("Sexta");
+        break;
+    case 7:
+        printf("Sabado");
+        break;
+    default:
+        printf("Numero invalido");
+        break;
+    }
+}
